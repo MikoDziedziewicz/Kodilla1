d1d7cc5a6daa8a98b6178beb61a94d76afff06e1
DESKTOP-GLADTPR\scarl <mikolajdziedziewicz@gmail.com>
2022-12-12 14:35:30 +0100

    Zadanie 1&2

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class BallComponent : MonoBehaviour
 {
 
+    public float scaleUpSpeed = 0.025f;
+ 
     
 
     // Start is called before the first frame update
     void Start()
     {
 
-     
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Debug.Log(" FPS: " + (1 / Time.deltaTime));
+
+
+        if (transform.localScale.y < 3.0f)
+        {
+            transform.localScale += Vector3.one * scaleUpSpeed;
+        }
+
+
+
     }
 }
