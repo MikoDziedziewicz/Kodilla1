c7a7e7778a88acb4f645352df7965cc82c3a3517
DESKTOP-GLADTPR\scarl <mikolajdziedziewicz@gmail.com>
2022-12-12 12:22:11 +0100

    Zadania FPS

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class BallComponent : MonoBehaviour
 {
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(" Time since last frame: " + Time.deltaTime);
+
+     
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Debug.Log(" FPS: " + (1 / Time.deltaTime));
     }
 }
