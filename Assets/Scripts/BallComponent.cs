c976ef0f0bfd9e6c4a2eab95beab2be26254d8a8
DESKTOP-GLADTPR\scarl <mikolajdziedziewicz@gmail.com>
2022-12-30 15:59:06 +0100

    Ćwiczenie: obrót w lewo, w prawo, zwiększanie/zmniejszanie

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -1,30 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class BallComponent : MonoBehaviour
 {
 
-    float minFPS = float.MaxValue;
-    float maxFPS = float.MinValue;
+   public enum BallInstruction
+    {
+        Idle = 0,
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        TurnLeft,
+        TurnRight,
+        ReduceSize,
+        IncreaseSize
+
+    }
+
+    public float Speed = 1.0f;
+    public float rotationSpeed = 100.0f;
+    private Vector3 vecRotation = Vector3.zero;
+
+    public BallInstruction Instruction = BallInstruction.Idle;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-       
+     
+        
     }
 
     // Update is called once per frame
     void Update()
     {
      
-     float currentFPS = 1 / Time.deltaTime;
-     
-     minFPS = Mathf.Min(minFPS, currentFPS);
-     maxFPS = Mathf.Max(maxFPS, currentFPS);
+        switch (Instruction)
+        {
+            case BallInstruction.MoveUp:
+                transform.position += Speed * Time.deltaTime * new Vector3(0, 1, 0);
+                break;
 
-     Debug.Log("FPS: " + currentFPS + " (Min: " + minFPS + ", Max: " + maxFPS + ")");
-     
+            case BallInstruction.MoveDown:
+                transform.position += Speed * Time.deltaTime * new Vector3(0, -1, 0);
+                break;
+
+            case BallInstruction.MoveLeft:
+                transform.position += Speed * Time.deltaTime * new Vector3(-1, 0, 0);
+                break;
+
+            case BallInstruction.MoveRight:
+                transform.position += Speed * Time.deltaTime * new Vector3(1, 0, 0);
+                break;
+
+            case BallInstruction.TurnLeft:
+                vecRotation += rotationSpeed * Time.deltaTime * new Vector3(0, -1, 0);
+                transform.rotation = Quaternion.Euler(vecRotation);
+                break;
+
+            case BallInstruction.TurnRight:
+                vecRotation += rotationSpeed * Time.deltaTime * new Vector3(0, 1, 0);
+                transform.rotation = Quaternion.Euler(vecRotation);
+                break;
+
+            case BallInstruction.IncreaseSize:
+                transform.localScale += Speed * Time.deltaTime * new Vector3(1, 1, 1);
+                break;
+
+            case BallInstruction.ReduceSize:
+                if (transform.localScale.x > 0)
+                {
+                    transform.localScale -= Speed * Time.deltaTime * new Vector3(1, 1, 1);
+                }
+                
+                break;
+             
+            default:
+                Debug.Log("Idle");
+                break;
+        }
 
     }
 
