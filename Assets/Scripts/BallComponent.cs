8f832d62172daaad69391ce67bb24eab0d0dbce4
DESKTOP-GLADTPR\scarl <mikolajdziedziewicz@gmail.com>
2023-01-01 17:16:21 +0100

    Zadanie: ruch

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -14,18 +14,19 @@ public class BallComponent : MonoBehaviour
         MoveRight,
         TurnLeft,
         TurnRight,
-        ReduceSize,
-        IncreaseSize
-
+        SizeDown,
+        SizeUp
     }
 
     public float Speed = 1.0f;
-    public float rotationSpeed = 100.0f;
-    private Vector3 vecRotation = Vector3.zero;
 
-    public BallInstruction Instruction = BallInstruction.Idle;
-
+    public List<BallInstruction> Instructions = new List<BallInstruction>();
     
+    private int CurrentInstruction = 0;
+    private float Distance = 0.0f;  
+    public float InstructionLenght = 3.0f;
+    
+    private Vector3 vecRotation = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -37,55 +38,72 @@ public class BallComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
-        switch (Instruction)
+        
+        if (CurrentInstruction < Instructions.Count)
         {
-            case BallInstruction.MoveUp:
-                transform.position += Speed * Time.deltaTime * new Vector3(0, 1, 0);
-                break;
+            
+            float RealSpeed = Speed * Time.deltaTime;
+            Distance += RealSpeed;
 
-            case BallInstruction.MoveDown:
-                transform.position += Speed * Time.deltaTime * new Vector3(0, -1, 0);
-                break;
+            switch (Instructions[CurrentInstruction])
+            {
+                case BallInstruction.MoveUp:
+                    transform.position += Vector3.up * RealSpeed;
+                    break;
 
-            case BallInstruction.MoveLeft:
-                transform.position += Speed * Time.deltaTime * new Vector3(-1, 0, 0);
-                break;
+                case BallInstruction.MoveDown:
+                    transform.position += Vector3.down * RealSpeed;
+                    break;
 
-            case BallInstruction.MoveRight:
-                transform.position += Speed * Time.deltaTime * new Vector3(1, 0, 0);
-                break;
+                case BallInstruction.MoveLeft:
+                    transform.position += Vector3.left * RealSpeed;
+                    break;
 
-            case BallInstruction.TurnLeft:
-                vecRotation += rotationSpeed * Time.deltaTime * new Vector3(0, -1, 0);
-                transform.rotation = Quaternion.Euler(vecRotation);
-                break;
+                case BallInstruction.MoveRight:
+                    transform.position += Vector3.right * RealSpeed;
+                    break;
 
-            case BallInstruction.TurnRight:
-                vecRotation += rotationSpeed * Time.deltaTime * new Vector3(0, 1, 0);
-                transform.rotation = Quaternion.Euler(vecRotation);
-                break;
+                case BallInstruction.SizeUp:
+                    transform.localScale += Vector3.one * RealSpeed;
+                    break;
 
-            case BallInstruction.IncreaseSize:
-                transform.localScale += Speed * Time.deltaTime * new Vector3(1, 1, 1);
-                break;
+                case BallInstruction.SizeDown:
+                    if (transform.localScale.x > 0)
+                    {
+                        transform.localScale -= Vector3.one * RealSpeed;
+                    }
 
-            case BallInstruction.ReduceSize:
-                if (transform.localScale.x > 0)
-                {
-                    transform.localScale -= Speed * Time.deltaTime * new Vector3(1, 1, 1);
-                }
-                
-                break;
-             
-            default:
-                Debug.Log("Idle");
-                break;
+                    break;
+
+                // Obrot o 180 stopni
+                case BallInstruction.TurnLeft:
+                    vecRotation += RealSpeed * new Vector3(0, -60, 0);
+                    transform.rotation = Quaternion.Euler(vecRotation);
+                    break;
+
+                case BallInstruction.TurnRight:
+                    vecRotation += RealSpeed * new Vector3(0, 60, 0);
+                    transform.rotation = Quaternion.Euler(vecRotation);
+                    break;
+
+                default:
+                    Debug.Log("Idle");
+                    break;
+            }
+
+            if (Distance > InstructionLenght)
+            {
+                Distance = 0.0f;
+                ++CurrentInstruction;
+            }
+
+         
         }
+    }
 
     }
 
 
 
 
-}
+
