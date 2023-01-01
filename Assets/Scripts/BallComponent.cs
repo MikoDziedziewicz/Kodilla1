495e0806ef1707924ca417dbaa65ba32da5ce3a6
DESKTOP-GLADTPR\scarl <mikolajdziedziewicz@gmail.com>
2023-01-01 18:36:43 +0100

    Input

--- Assets/Scripts/BallComponent.cs
+++ Assets/Scripts/BallComponent.cs
@@ -5,28 +5,7 @@ using System;
 public class BallComponent : MonoBehaviour
 {
 
-   public enum BallInstruction
-    {
-        Idle = 0,
-        MoveUp,
-        MoveDown,
-        MoveLeft,
-        MoveRight,
-        TurnLeft,
-        TurnRight,
-        SizeDown,
-        SizeUp
-    }
-
-    public float Speed = 1.0f;
-
-    public List<BallInstruction> Instructions = new List<BallInstruction>();
-    
-    private int CurrentInstruction = 0;
-    private float Distance = 0.0f;  
-    public float InstructionLenght = 3.0f;
-    
-    private Vector3 vecRotation = Vector3.zero;
+    public float BallSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -38,70 +17,48 @@ public class BallComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (CurrentInstruction < Instructions.Count)
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            transform.position += Vector3.up * BallSpeed;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
             
-            float RealSpeed = Speed * Time.deltaTime;
-            Distance += RealSpeed;
+                
 
-            switch (Instructions[CurrentInstruction])
-            {
-                case BallInstruction.MoveUp:
-                    transform.position += Vector3.up * RealSpeed;
-                    break;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * BallSpeed;
+            transform.localScale = new Vector3(1, -1, 1);
+        }
+          
 
-                case BallInstruction.MoveDown:
-                    transform.position += Vector3.down * RealSpeed;
-                    break;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * BallSpeed;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+            
 
-                case BallInstruction.MoveLeft:
-                    transform.position += Vector3.left * RealSpeed;
-                    break;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * BallSpeed;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+            
 
-                case BallInstruction.MoveRight:
-                    transform.position += Vector3.right * RealSpeed;
-                    break;
-
-                case BallInstruction.SizeUp:
-                    transform.localScale += Vector3.one * RealSpeed;
-                    break;
-
-                case BallInstruction.SizeDown:
-                    if (transform.localScale.x > 0)
-                    {
-                        transform.localScale -= Vector3.one * RealSpeed;
-                    }
-
-                    break;
-
-                // Obrot o 180 stopni
-                case BallInstruction.TurnLeft:
-                    vecRotation += RealSpeed * new Vector3(0, -60, 0);
-                    transform.rotation = Quaternion.Euler(vecRotation);
-                    break;
-
-                case BallInstruction.TurnRight:
-                    vecRotation += RealSpeed * new Vector3(0, 60, 0);
-                    transform.rotation = Quaternion.Euler(vecRotation);
-                    break;
-
-                default:
-                    Debug.Log("Idle");
-                    break;
-            }
-
-            if (Distance > InstructionLenght)
-            {
-                Distance = 0.0f;
-                ++CurrentInstruction;
-            }
-
-         
+        if (Input.GetKeyDown(KeyCode.Space) & transform.localScale.x < 2)
+        {
+            transform.localScale = Vector3.one * 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) & transform.localScale.x != 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
         }
     }
 
-    }
+}
 
 
 
