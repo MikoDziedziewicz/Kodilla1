9128531c57b86a8397c511eba2149b6769d35415
DESKTOP-GLADTPR\scarl <mikolajdziedziewicz@gmail.com>
2023-02-26 22:01:46 +0100

    Unity ads

---
+++ Assets/AdsManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    private const string ANDROID_AD_ID = "5180792";
+    private bool testMode = true;
+    private string bannerID = "banner";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(ANDROID_AD_ID, testMode);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    private IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Show(bannerID);
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+
+    }
+}
